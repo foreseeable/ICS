d1208732a54b5c227c3c69cf2cd9db22ad5b59bd
foreseeable <wcmg.chen@gmail.com>
2016-09-18 23:39:57 +0800

    看错题了

--- HW1/2.61.c
+++ HW1/2.61.c
@@ -6,23 +6,23 @@
 #include <assert.h>
 #include <stdio.h>
 
-int calcA(unsigned x) { return !(x + 1); }
-int calcB(unsigned x) { return !x; }
+int calcA(unsigned x) { return x > 0; }
+int calcB(unsigned x) { return (~x) > 0; }
 int calcC(unsigned x) {
     x &= (1 << 8) - 1;
-    return !(x + 1);
+    return (x > 0);
 }
 int calcD(unsigned x) {
     int w = sizeof(int) << 3;
-    return !(x >>= (w - 8));
+    return (~(x >> (w - 8))) > 0;
 }
 int main() {
     unsigned x;
     scanf("%u", &x);
-    int A = !(x + 1);  // A : all bit of x equals 1
-    int B = !x;        // B : all bit of x euqals 0
+    int A = !(x + 1);  // A : any bit of x equals 1
+    int B = !x;        // B : any bit of x euqals 0
     int C =
-        calcC(x);  // C : all bit in the least significant byte of x equals 1
-    int D = calcD(x);  // D : all bit in the most significant byte of x equal 0
+        calcC(x);  // C : any bit in the least significant byte of x equals 1
+    int D = calcD(x);  // D : any bit in the most significant byte of x equal 0
     printf("%d %d %d %d\n", A, B, C, D);
 }
